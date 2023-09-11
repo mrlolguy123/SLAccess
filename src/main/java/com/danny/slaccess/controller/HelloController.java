bbb75b3ec1c63285ee9e626124eaa32484c75bd5
Danny Le <24dannle@gmail.com>
2023-09-10 22:08:36 -0400

    Turns out I misread the project details.
    I created the Access Table with just the user ID and Scan In Time because that's how the actual PSU Harrisburg Sun Lab functions.
    
    However, the projected wanted an In AND Out time, so I had to fix the login screen and update the tables as needed.

--- src/main/java/com/danny/slaccess/controller/HelloController.java
+++ src/main/java/com/danny/slaccess/controller/HelloController.java
@@ -11,7 +11,6 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.sql.*;
 import java.time.LocalDateTime;
-import java.util.concurrent.TimeUnit;
 
 public class HelloController {
     Database db = new Database();
@@ -31,7 +30,7 @@ public class HelloController {
     private Label loginMessage;
 
     @FXML
-    protected void loginButtonPressed () throws InterruptedException {
+    protected void loginButtonPressed () throws SQLException {
         String initial = userIDField.getText();
         String user = "";
 
@@ -96,7 +95,7 @@ public class HelloController {
         }
         passField.setVisible(true);
         passLabel.setVisible(true);
-        LoginButton.setText("Login");
+        LoginButton.setText("Scan");
     }
 
     private boolean isNumeric(String a) {
@@ -109,36 +108,43 @@ public class HelloController {
         return true;
     }
 
-    private void enterDB(int id)
+    private void enterDB(int id) throws SQLException
     {
         String temp = LocalDateTime.now().toString();
         String now = temp.substring(0,10) + " " + temp.substring(11,19);
         System.out.println(temp);
 
-        String queryCheck = "SELECT * FROM userTable WHERE numID=?";
-        String queryEntry = "INSERT INTO accessTable (numID, inTime) VALUES (?, ?);";
+        String queryCheckState = "SELECT * FROM accessTable WHERE numID=? AND outTime IS NULL";
+        String queryCheckAuth = "SELECT * FROM userTable WHERE numID=?";
+        String UpdateEntry1 = "INSERT INTO accessTable (numID, inTime, outTime) VALUES (?, ?, null);";
+        String UpdateEntry2 = "UPDATE accessTable SET outTime = ? WHERE numID = ? AND outTime IS NULL;";
 
-        try {
-            PreparedStatement ps = con.prepareStatement(queryCheck);
-            ps.setInt(1,id);
-            ResultSet rs = ps.executeQuery();
-            if(rs.getInt(1) == id && rs.getBoolean(4)) {
-                ps = con.prepareStatement(queryEntry);
-                ps.setInt(1,id);
-                ps.setString(2,now);
+        PreparedStatement ps = con.prepareStatement(queryCheckState);
+        ps.setInt(1, id);
+        ResultSet rs = ps.executeQuery();
+
+        if(rs.getInt(1) == 0) { // Run if Student is outside of Sun Lab (when outTime is not null)
+            ps = con.prepareStatement(queryCheckAuth);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            if (rs.getInt(1) == id && rs.getBoolean(4)) {
+                ps = con.prepareStatement(UpdateEntry1);
+                ps.setInt(1, id);
+                ps.setString(2, now);
                 ps.executeUpdate();
                 loginMessage.setText("Your entry to the Sun Lab has been recorded.");
+            } else {
+                loginMessage.setText("You are not currently authorized to enter. Contact an administrator for help.");
             }
-            else {
-                loginMessage.setText("You are not currently authorized to enter. See an Administrator.");
-            }
-        } catch (SQLException e) {
-            System.out.println(e);
         }
-    }
+        else { // Run if student is currently in the lab (outTime IS NULL)
+            ps = con.prepareStatement(UpdateEntry2);
+            ps.setString(1, now);
+            ps.setInt(2, id);
+            ps.executeUpdate();
+            loginMessage.setText("Your exit from the Sun Lab has been recorded.");
+        }
 
-    private void timeoutMessage() throws InterruptedException {
-        TimeUnit.SECONDS.sleep(3);
-        loginMessage.setText("");
+
     }
 }
